333f4f96f09a35a0f0d0c3e7990aa8a6bf163755
Andrew Beers <andybeers@gmail.com>
2016-11-28 14:10:10 -0800

    Added GET-all route and GET-single route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./lib/app');
 const port = process.env.PORT || 3000;
-require('.lib/setup-mongoose');
+require('./lib/setup-mongoose');
 
 const server = http.createServer(app);
 
