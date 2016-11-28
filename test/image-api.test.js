6548ea3a0ef9f5a193ba0adaf7f7155048da3b76
Nathan Pickard <nathan.pickard87@gmail.com>
2016-11-28 14:33:03 -0800

    Adding quiz code

---
+++ test/image-api.test.js
@@ -0,0 +1,18 @@
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+// const expect = chai.expect;
+chai.use(chaiHttp);
+
+const connection = require('../lib/setup-mongoose');
+const app = require('../lib/app');
+
+describe('Image', () => {
+  before(done => {
+    const drop = () => connection.db.dropDatabase(done);
+    if(connection.readyState === 1) drop();
+    else connection.on('open', drop);
+  });
+
+  const request = chai.request(app);
+  })
+})
