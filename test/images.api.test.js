0cd27de404be917d5dbd9a0761e976470271b967
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-11-28 14:35:18 -0800

    Initial commit after writing tests and tests passing so far

---
+++ test/images.api.test.js
@@ -0,0 +1,48 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+const connection = require('../lib/setup-mongoose');
+const app = require('../lib/app');
+
+describe ('images API E2E testing', () => {
+
+  const test_image = { // eslint-disable-line no-unused-vars
+    title: 'Attack Cat',
+    description: 'Incredibly scary cat',
+    category: 'animals',
+    url: 'www.attackcat.com/attackcat.jpg'
+  };
+
+  before((done) => {
+    const CONNECTED = 1;
+    if (connection.readyState === CONNECTED) dropCollection();
+    else connection.on('open', dropCollection);
+
+    function dropCollection() {
+      const name = 'images';
+      connection.db
+        .listCollections({ name })
+        .next((err, collInfo) => {
+          if (!collInfo) return done();
+          connection.db.dropCollection(name, done);
+        });
+    }
+  });
+
+  const request = chai.request(app);
+
+  it ('GET /images should return empty array', (done) => {
+    request
+      .get('/images')
+      .then((res) => {
+        expect(res.body).to.deep.equal([]);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+});
